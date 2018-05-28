b300cdd12d5062031154214876077cbe05527f4b
Johannes Graeter <johannes.graeter@googlemail.com>
2018-05-28 15:35:07 +0200

    Install with catkin_tools

--- install_repos.sh
+++ install_repos.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
+# make catkin profile
+catkin config --profile limo_release -x _limo_release --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wall -Wextra -Wno-unused-parameter -Werror=address -Werror=array-bounds=1 -Werror=bool-compare -Werror=comment -Werror=enum-compare -Werror=format -Werror=init-self -Werror=logical-not-parentheses -Werror=maybe-uninitialized -Werror=memset-transposed-args -Werror=nonnull -Werror=nonnull-compare -Werror=openmp-simd -Werror=parentheses -Werror=return-type -Werror=sequence-point -Werror=sizeof-pointer-memaccess -Werror=switch -Werror=tautological-compare -Werror=trigraphs -Werror=uninitialized -Werror=volatile-register-var
+
+# clone stuff
 cd ..
 git clone https://github.com/KIT-MRT/feature_tracking.git
 git clone https://github.com/johannes-graeter/mono_lidar_depth.git
 git clone https://github.com/KIT-MRT/viso2.git
 git clone https://github.com/KIT-MRT/mrt_cmake_modules.git
-# This hack is needed since rosinterface_handler has problems beeing 
-# imported by raw catkin
-cd ..
-catkin_make > /tmp/first_build_bound_to_fail.txt
-cd src
 git clone https://github.com/KIT-MRT/rosinterface_handler.git
-cd ..
-catkin_make
+
+# build it with profile
+catkin build --profile limo_release
